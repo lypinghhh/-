907ae077772021583457e8e06e6cd6164b22dcea
LYP <1097404125@qq.com>
2020-02-29 10:55:18 +0800

    更新

--- algorithm/leetcode/26-ez-删除排序数组中重复项.js
+++ algorithm/leetcode/26-ez-删除排序数组中重复项.js
@@ -3,12 +3,14 @@
 var removeDuplicates = function (nums) {
     var cur = nums[0];
     for (var i = 1; i < nums.length;) {
+        console.log(i)
         if (nums[i] === cur)
             nums.splice(i, 1);
+            
         else
            //新插入元素与当前元素不相同，则移动cur指到当前i元素
             cur = nums[i++];
     }
     return nums.length
 };
-
+removeDuplicates([1,1,1,1,3,4])
